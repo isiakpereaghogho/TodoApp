3052895078a8e660be44b811b5e2867032c8d729
ïsiakpereaghogho <aghososafe@yahoo.com>
2021-06-21 18:03:48 +0100

    Initial commit

---
+++ app/src/main/java/com/aghogho/to_doapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.aghogho.to_doapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    val username:" "
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
